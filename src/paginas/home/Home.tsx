1f4d7085665d9b16b45bcfc1a04670e1376edf93
Mauricio <mauricio_oliver75@live.com>
2022-05-26 20:33:14 -0300

    Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1>Home</h1>
+            <img src="Formula 1.jpg" alt="Fórmula 1" />
+        </>
+    );
+}
+
+export default Home;
