c824041168241cf8c9667bfaf92f7502604a3771
Mauricio <mauricio_oliver75@live.com>
2022-05-30 21:41:48 -0300

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './componets/estaticos/navbar/Navbar';
 import Footer from './componets/estaticos/footer/Footer';
 import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-        <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
