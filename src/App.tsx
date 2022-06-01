1cd68d151fbeb7c3f831ad94fde4d7830f3170e9
Mauricio <mauricio_oliver75@live.com>
2022-06-01 11:53:39 -0300

    getAll

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
 import './App.css';
+import ListaTema from './componets/temas/listatema/ListaTema';
+import ListaPostagem from './componets/postagens/listapostagem/ListaPostagem';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/cadastro" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/posts" element={<ListaPostagem />} />
         </Routes>
       </div>
       <Footer />
